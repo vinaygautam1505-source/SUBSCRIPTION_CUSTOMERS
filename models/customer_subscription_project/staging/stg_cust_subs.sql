13064222b5ac1c46dd5fc1dc0b465df8a699e8a8
vinaygautam1505-source <vinaygautam1505@gmail.com>
2026-02-07 16:17:30 +0530

    Update stg_cust_subs.sql

--- models/customer_subscription_project/staging/stg_cust_subs.sql
+++ models/customer_subscription_project/staging/stg_cust_subs.sql
@@ -1,27 +1,25 @@
-with source as (
 
-    select *
-    from {{ source('customer_subscription', 'SHEET1') }}
 
-),
-
-renamed as (
+with cleaned_and_renamed as (
 
     select
-        nullif(trim("CITY"), '')                     as city,
-        nullif(trim(lower("EMAIL")), '')             as email,
-        nullif(trim("INDEX"), '')                    as index_col,
-        nullif(trim("COMPANY"), '')                  as company,
-        nullif(trim("COUNTRY"), '')                  as country,
-        nullif(trim("PHONE 1"), '')                  as phone_1,
-        nullif(trim("PHONE 2"), '')                  as phone_2,
-        nullif(trim("WEBSITE"), '')                  as website,
-        nullif(trim("LAST NAME"), '')                as last_name,
-        nullif(trim("FIRST NAME"), '')               as first_name,
-        nullif(trim("CUSTOMER ID"), '')              as customer_id,
-        try_to_date(nullif(trim("SUBSCRIPTION DATE"), '')) as subscription_date
-    from source
+        trim("CITY")                     as city,
+        trim(lower(coalesce("EMAIL", 'abc@gmail.com')))            as email,
+        trim("INDEX")                    as index_col,
+        trim("COMPANY")                  as company,
+        trim("COUNTRY")                  as country,
+        REGEXP_REPLACE(trim(COALESCE("PHONE 1", '0000000000')), '[^0-9]', '') AS phone_1,
+        REGEXP_REPLACE(trim(COALESCE("PHONE 2", '0000000000')), '[^0-9]', '') AS phone_2,
+        trim(lower("WEBSITE"))                  as website,
+        trim(coalesce("LAST NAME", 'NA'))                as last_name,
+        trim(coalesce("FIRST NAME", 'NA') )              as first_name,
+        trim(coalesce("CUSTOMER ID", '00') )             as customer_id,
+        try_to_date(trim("SUBSCRIPTION DATE"), 'YYYY-MM-DD') as subscription_date
 
+    FROM {{ source('customer_subscription', 'sheet1') }}
 )
+SELECT *
+FROM cleaned_and_renamed
+
 
 select * from renamed
