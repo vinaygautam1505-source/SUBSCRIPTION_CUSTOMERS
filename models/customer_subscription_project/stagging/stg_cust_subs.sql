278bcd39ab79bf170d047d71131bf36e01b6db9f
vinaygautam1505-source <vinaygautam1505@gmail.com>
2026-02-07 12:18:16 +0530

    Create stg_cust_subs.sql

---
+++ models/customer_subscription_project/stagging/stg_cust_subs.sql
@@ -0,0 +1,25 @@
+WITH SOURCE AS(
+  SELECT * FROM {{source('customer_subscription_project', 'SHEET1')}} ),
+
+  
+RENAMED AS ( 
+  
+  SELECT
+  
+NULLIF(TRIM("CITY"), '')  AS city,
+NULLIF(TRIM(LOWER("EMAIL")), '') AS email,
+NULLIF(TRIM("INDEX"),'') AS index_col,
+NULLIF(TRIM("COMPANY"),'') AS company,
+NULLIF(TRIM("COUNTRY"), '') AS country,
+NULLIF(TRIM("PHONE 1"), '') AS phone_1,
+NULLIF(TRIM("PHONE 2"),'') AS phone_2,
+NULLIF(TRIM("WEBSITE"), '') AS website,
+NULLIF(TRIM("LAST NAME"),'') AS last_name,
+NULLIF(TRIM("FIRST NAME"), '') AS first_name,
+NULLIF(TRIM("CUSTOMER ID"), '') AS customer_id,
+NULLIF(TRIM("SUBSCRIPTION DATE"), '')::DATE AS subscription_date
+
+  FROM SOURCE )
+  
+  SELECT * FROM RENAMED;
+
